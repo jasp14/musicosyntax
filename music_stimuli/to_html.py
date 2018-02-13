48e660ae71d24907fd5c518fb9707226efeb9eb2
julianoks <juao15@hampshire.edu>
2018-02-13 15:05:38 -0500

    script

---
+++ music_stimuli/to_html.py
@@ -0,0 +1,20 @@
+import os
+
+INPUT_FOLDER = "wav"
+OUTPUT_FOLDER = "html"
+
+if __name__ == '__main__':
+	if not os.path.isdir(INPUT_FOLDER): os.mkdir(OUTPUT_FOLDER)
+	for file_name in os.listdir(INPUT_FOLDER):
+		canonical = file_name.split(".")[0]
+		content = \
+'''<!DOCTYPE html>
+<html>
+<body>
+<audio class = 'audio-message' autoplay>
+<source src='../''' + INPUT_FOLDER + '''/''' + file_name + '''' type='audio/wav'>
+</audio>
+</body>
+</html>
+'''
+		open(os.path.join(OUTPUT_FOLDER, canonical+".html"), 'w').write(content)
